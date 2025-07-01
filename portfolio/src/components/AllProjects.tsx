199026e77410fe5b5125d7856f05c174fc6a5fe3
Timothy Campbell <tcampbel22@gmail.com>
2025-07-01 19:10:11 +0300

    fixed some light mode issues and updated about me text

--- portfolio/src/components/AllProjects.tsx
+++ portfolio/src/components/AllProjects.tsx
@@ -14,11 +14,12 @@ const Project = ({ projectName, link, image }: ProjectProps) => {
 			style={{backgroundImage: `url(${image})`}}
 			className='bg-cover bg-center w-full h-48 md:h-96
 				bg-gray-700 shadow-md mb-1 text-xl md:text-4xl
-				font-bold text-gray-900 dark:text-gray-100
+				font-bold text-gray-100
 				transform hover:scale-105 transition-all duration-300 ease-in-out
 				flex items-center justify-center'
 				>
-				{projectName}
+				<span className="relative z-10 absolute inset-0 bg-gray-600/60 rounded p-1 backdrop-blur-sm ">{projectName}</span>
+
 		</a>
 	);
 };
--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -14,11 +14,12 @@ const Project = ({ projectName, link, image }: ProjectProps) => {
 			style={{backgroundImage: `url(${image})`}}
 			className='bg-cover bg-center w-full h-48 md:h-96
 				bg-gray-200 shadow-md mb-1 text-xl md:text-4xl
-				font-bold text-gray-900 dark:text-gray-100
+				font-bold text-gray-100 text-shadow dark:text-gray-100
 				transform hover:scale-105 transition-all duration-300 ease-in-out
 				flex items-center justify-center'
 				>
-				{projectName}
+				<span className="relative z-10 absolute inset-0 bg-gray-600/60 rounded p-1 backdrop-blur-sm ">{projectName}</span>
+				
 		</a>
 	);
 };
@@ -30,19 +31,11 @@ export const Projects:React.FC = ()  => {
 			<div className='grid grid-cols-1 lg:grid-cols-2 gap-10 p-5 m-5 '>
 				<Project projectName="Shell interpreter" link="https://github.com/tcampbel22/42_hive_minishell" image="/shell.gif"/>
 				<Project projectName="2.5D raycasting game" link="https://github.com/tcampbel22/cub3d" image="/duck.gif"/>
-				{/* <Project projectName="Custom C library" link="https://github.com/tcampbel22/42_hive_libft"/> */}
-				{/* <Project projectName="Dining philosophers" link="https://github.com/tcampbel22/42_philosophers"/> */}
 				<Project projectName="2D Platformer" link="https://github.com/tcampbel22/42_hive_so_long" image="/dino.gif"/>
-				{/* <Project projectName="Sorting Algorithm" link="https://github.com/tcampbel22/42_hive_push_swap"/> */}
-				{/* <Project projectName="Pong web app" link="https://github.com/tcampbel22/transcendence"/> */}
 				<Project projectName="HTTP 1.1 server" link="https://github.com/tcampbel22/42_webserv" image="server.gif"/>
-				{/* <Project projectName="Custom printf" link="https://github.com/tcampbel22/42_hive_ft_printf"/> */}
-				{/* <Project projectName="Meal planning app" link=""/> */}
-				{/* <Project projectName="Exercise tracker app" link="https://github.com/tcampbel22/Exercise-Tracker-Dashboard"/> */}
-				{/* <Project projectName="Card shuffler app" link="https://tarot-tim-campbell.onrender.com"/> */}
 			</div>
 			<div className='flex items-center justify-center'>
-				<Link to="/projects" className='text-center dark:text-white p-4 bg-gray-800 dark:hover:bg-gray-600 transform hover:scale-110 transition-all duration-300 ease-in-out'>See more projects</Link>
+				<Link to="/projects" className='text-center dark:text-white p-4 border-2 dark:bg-gray-800 dark:hover:bg-gray-600 transform hover:scale-110 transition-all duration-300 ease-in-out'>See more projects</Link>
 			</div>
 		</div>
 	);
