4d64f48e30618db1a0666e74a1332987f9320bd4
Timothy Campbell <tcampbel22@gmail.com>
2025-07-02 15:56:01 +0300

    Trying to get li to work

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -12,6 +12,8 @@ const HeaderButton = ({ text, link, image, internal }: HeaderButtonProps) => {
 	return !internal ? (
 			<a  href={link} 
 				title={text}
+				target="_blank" 
+				rel="noopener noreferrer"
 				style={{ backgroundImage: image ? `url(${image})` : undefined }} 
 				className={`inline-block mb-4 w-16 h-16 bg-cover bg-white outline-black dark:outline-none dark:bg-gray-950 rounded-md 
 				 transform transition-all duration-300 hover:scale-110 relative hover-shadow-sm ease-in-out`}
@@ -51,11 +53,10 @@ export const Header:React.FC = () => {
 
 				<div className='space-x-4'>
 					<HeaderButton text="Github" link="https://github.com/tcampbel22" image={!darkMode ? '/portfolio_page/github-mark.png' : '/portfolio_page/github-mark-white.png'}/>
-					<HeaderButton text="LinkedIn" link="https://www.linkedin.com/in/tim-campbell-49b40880" image={!darkMode ? '/portfolio_page/LI-Blue.png' : '/portfolio_page/LI-White.png'}/>
+					<HeaderButton text="LinkedIn" link="https://www.linkedin.com/in/tim-campbell-49b40880?trk=profile-badge" image={!darkMode ? '/portfolio_page/LI-Blue.png' : '/portfolio_page/LI-White.png'}/>
 					<HeaderButton text="Resume" link="/resume" image={!darkMode ? '/portfolio_page/cv-black.png' : '/portfolio_page/cv-colour.png'} internal={true}/>
 				</div>
 			</div>
 
 	);
 };
-
