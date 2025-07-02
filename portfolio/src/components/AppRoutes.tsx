3d2c9341e1c14a9d6e2a62ca5ea4fbd60d209b93
Timothy Campbell <tcampbel22@gmail.com>
2025-07-02 15:28:57 +0300

    Updates remainig img paths

--- portfolio/src/components/AppRoutes.tsx
+++ portfolio/src/components/AppRoutes.tsx
@@ -7,7 +7,7 @@ import { AllProjects } from "./AllProjects";
 export const AppRoutes:React.FC = () => {
 	return (
 		<Routes>
-			<Route path="/resume" element={<Resume />}/>
+			<Route path="/portfolio_page/resume" element={<Resume />}/>
 			<Route path="/projects" element={<AllProjects />}/>
 			<Route path="/portfolio_page" element={<HomePage />}/>
 		</Routes>
--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -5,9 +5,9 @@ export const Footer:React.FC = () => {
 		<div className="flex flex-col dark:text-white text-sm font-bold mt-20">
 			<h1 className="text-center mb-2">Built by me 2025</h1>
 			<div className="flex flex-row justify-center items-center mb-4 space-x-2">
-				<img src="/react.svg" title="React"></img>
-				<img src="/tailwindcss.png" title="TailwindCSS"></img>
-				<img src="ts-logo-128.png" title="Typescript"></img>
+				<img src="/portfolio_page/react.svg" title="React"></img>
+				<img src="/portfolio_page/tailwindcss.png" title="TailwindCSS"></img>
+				<img src="/portfolio_page/ts-logo-128.png" title="Typescript"></img>
 			</div>
 			<a href="https://www.flaticon.com/free-icons/curriculum-vitae" title="cv icon" className="text-center">CV icon created by kerismaker</a>
 		</div>
