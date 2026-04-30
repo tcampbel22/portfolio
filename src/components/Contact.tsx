5b26bb4ee315fb325cd6cf207482cbdfdfc8e563
tim <tim@deb12.deb12>
2026-04-30 16:12:33 +0300

    Update contact info and pdf metadata

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HeaderCenter, HeaderMini } from "./HeaderText";
-import { PhoneIcon, EnvelopeIcon, PaperPlaneTiltIcon } from "@phosphor-icons/react"
+import { EnvelopeIcon } from "@phosphor-icons/react"
 import type { DarkProps } from "../types/types";
 import { NavigateButton } from "./Utils";
 
@@ -8,33 +8,23 @@ export const Contact:React.FC<DarkProps> = ({ isDark }) => {
 	return (
 		<div className="pt-5 md:py-8">
 			<HeaderCenter text="Contact" />
-			<div className="flex flex-col md:flex-row items-center md:justify-between pt-6 md:pt-0">
-				<div className="flex flex-col">
-					<div className="flex justify-start items-center space-x-8 py-2">
-						<PhoneIcon size={64} color={isDark ? '#f6f5f4' : '#000000'}/>
-						<HeaderMini text="+358 45 78769676"/>
+			<div className="flex flex-col items-center pt-6 md:pt-0">
+				<div className="flex flex-col items-center">
+					<p className="text-md md:text-lg dark:text-gray-100 text-center px-4 mt-5 md:pt-0 md:text-left max-w-3xl">
+						If you want to reach out regarding recruitment feel free to check out my resume below.
+					</p>
+					<div className="flex justify-center items-center md:my-15 my-10">
+						<NavigateButton link="/resume" text="Read CV"/>
 					</div>
-					<div className="flex justify-start items-center space-x-8 py-2">
+				
+					<p className="text-md md:text-lg dark:text-gray-100 text-center px-4 my-5 md:pt-0 md:text-left max-w-3xl">
+						For anything else just drop me an email.
+					</p>	
+					<div className="flex flex-col md:flex-row justify-center items-center md:space-x-8 pt-2">
 						<EnvelopeIcon size={64} color={isDark ? '#f6f5f4' : '#000000'}/>
 						<HeaderMini text="mail@timcampbell.xyz"/>
 					</div>
-					<div className="flex justify-start items-center space-x-8 py-2">
-						<PaperPlaneTiltIcon size={64} alt="Telegram" color={isDark ? '#f6f5f4' : '#000000'}/>
-						<HeaderMini text="@tcampbel22"/>
-					</div>
 				</div>
-				<div className="flex flex-col items-end">
-					<p className="text-md md:text-lg dark:text-gray-100 text-center px-4 mt-5 md:pt-0 md:text-left max-w-3xl">If you want to reach out regarding recruitment I'm currently looking for full-stack,
-						backend and frontend positions or anything web related. Feel free to check out my resume below.
-						<br/>
-						<br/>
-						For anything else just drop me a text or send me an email.
-					</p>
-			
-				</div>
-			</div>
-			<div className="flex justify-center items-center md:mt-6 mt-10">
-				<NavigateButton link="/resume" text="Read CV"/>
 			</div>
 		</div>
 	);
